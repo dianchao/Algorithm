af6f13544a5b4a1477783123f6c4eaa732a8025a
zdc <947389057@qq.com>
2019-11-30 21:13:51 +0800

    回文字符串

---
+++ src/com/dianchao/leetcode/Palindrome.java
@@ -0,0 +1,33 @@
+package com.dianchao.leetcode;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str = "ABCBa";
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome(str));
+    }
+
+    private boolean isAlphanumeric(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+                || (c >= '0' && c <= '9');
+    }
+
+    private boolean isEqualIgnoreCase(char a, char b) {
+        if (a >= 'A' && a <= 'Z') a += 32;
+        if (b >= 'A' && b <= 'Z') b += 32;
+        return a == b;
+    }
+
+    // Time: O(n), Space: O(1)
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+
+        int i = 0, j = s.length() - 1;
+        for (; i < j; ++i, --j) {
+            while (i < j && !isAlphanumeric(s.charAt(i))) ++i;
+            while (i < j && !isAlphanumeric(s.charAt(j))) --j;
+            if (i < j && !isEqualIgnoreCase(s.charAt(i), s.charAt(j))) return false;
+        }
+        return true;
+    }
+}
